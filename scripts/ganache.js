3f81e5ca180175fb1fa394b73a0651b0fd1f7e82
Justin <justinas.zaliaduonis@gmail.com>
2021-05-16 22:21:52 +0200

    ganache.js url correction

--- scripts/ganache.js
+++ scripts/ganache.js
@@ -1,5 +1,5 @@
 const ganache = require("ganache-core");
 require('dotenv').config();
 
-const server = ganache.server({ fork: process.env.KOVAN_INFURA_URL, gasLimit: 12.5e6, gasPrice: 1e6, unlocked_accounts: [0, 1], logger: console });
+const server = ganache.server({ fork: process.env.MAINNET_INFURA_URL, gasLimit: 12.5e6, gasPrice: 1e6, unlocked_accounts: [0, 1], logger: console });
 server.listen(8545);
