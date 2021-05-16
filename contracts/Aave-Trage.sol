ae6a4629f446df6570c809f76c518cc38467098a
Justin <justinas.zaliaduonis@gmail.com>
2021-05-16 20:33:40 +0200

    Add guap function

--- contracts/Aave-Trage.sol
+++ contracts/Aave-Trage.sol
@@ -1,9 +1,10 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import { ILendingPool } from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import { ILendingPool } from '@aave/protocol-v2/contracts/interfaces/ILendingPool.sol';
 import { DataTypes } from '@aave/protocol-v2/contracts/protocol/libraries/types/DataTypes.sol';
-
+import { IERC20 } from '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
+import { IUniswapV2Router02 } from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 
 contract AaveTrage {
 
@@ -41,5 +42,31 @@ contract AaveTrage {
         }
     }
 
+    function guap(address collateral, uint256 amount, address borrowedAsset, uint256 amountToBorrow, address arbitrageAsset) external {
+        _deposit(collateral, amount);
+        _borrow(borrowedAsset, amountToBorrow);
 
+        uint256 borrowedBalance = IERC20(borrowedAsset).balanceOf(address(this));
+        _swap(borrowedAsset, arbitrageAsset, borrowedBalance);
+
+        uint256 arbitrageBalance = IERC20(arbitrageAsset).balanceOf(address(this));
+        _deposit(arbitrageAsset, arbitrageBalance);
+    }
+
+    function _deposit(address collateral, uint256 amount) internal {
+        IERC20(collateral).approve(aave, amount);
+        ILendingPool(aave).deposit(collateral, amount, address(this), 0);
+    }
+
+    function _borrow(address assetToBorrow, uint256 amount) internal {
+        ILendingPool(aave).borrow(assetToBorrow, amount, 2, 0, address(this));
+    }
+
+    function _swap(address from, address to, uint256 amount) internal returns (uint256 balance) {
+        IERC20(from).approve(uniswap, amount);
+        address[] memory path = new address[](2);
+        path[0] = from;
+        path[1] = to;
+        IUniswapV2Router02(uniswap).swapExactTokensForTokens(amount, 0, path, address(this), block.timestamp + 10 ** 2);
+    }
 }
--- test/test_aavetrage.js
+++ test/test_aavetrage.js
@@ -1,5 +1,6 @@
 const AaveTrage = artifacts.require('AaveTrage');
 const uniswap = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
+const weth = require('@uniswap/v2-periphery/build/ERC20.json');
 const aave = require('@aave/protocol-v2/artifacts/contracts/protocol/lendingpool/LendingPool.sol/LendingPool.json');
 const _ = require('lodash')
 
@@ -48,4 +49,22 @@ contract('Aave-Trage', accounts => {
         assert.equal(bestBorrowToken.toLowerCase(), borrowManual.toLowerCase(), 'Borrow tokens mismatch!');
         assert.equal(bestSupplyToken.toLowerCase(), supplyManual.toLowerCase(), 'Supply tokens mismatch!');
     });
+
+    it('Should perform guap and shut correctly', async () => {
+        const wrappedETHaddress = await UNISWAP.methods.WETH().call();
+        const daiAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        const chainLink = '0x514910771af9ca656af840dff83e8264ecf986ca';
+        const TEST_AMOUNT = web3.utils.toWei('1', "ether");
+        const AMOUNT_TO_BORROW = web3.utils.toWei('2000', 'ether');
+
+        //Convert ETH to WETH
+        const WETH = await new web3.eth.Contract(weth.abi, wrappedETHaddress);
+        await web3.eth.sendTransaction({from: user1, to: wrappedETHaddress, value:TEST_AMOUNT});
+        await WETH.methods.transfer(AAVETRAGE.address, TEST_AMOUNT).send({from: user1});
+
+        await AAVETRAGE.guap(wrappedETHaddress, TEST_AMOUNT, daiAddress, AMOUNT_TO_BORROW, chainLink, {from: user1});
+
+
+
+    })
 });
