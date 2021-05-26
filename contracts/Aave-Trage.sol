5a3d41b94b5b57360fce363463c5ab9e758539b4
JustinZal <38131575+JustinZal@users.noreply.github.com>
2021-05-26 16:06:38 +0200

    feature/aavetrage (#1)
    * Add packages
    
    * Add peek function in contract + peek function tests
    
    * Add guap function
    
    * Add shut method + tests
    
    * Restructure folders and add truffle config file
    
    * Cosmetic changes and add package-lock.json
    
    * add minor contract fixes
    
    * ganache.js url correction
    
    * Add small fix for rerunning tests
    
    * Fix reacurring transfer method

---
+++ contracts/Aave-Trage.sol
@@ -0,0 +1,95 @@
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import { ILendingPool } from '@aave/protocol-v2/contracts/interfaces/ILendingPool.sol';
+import { DataTypes } from '@aave/protocol-v2/contracts/protocol/libraries/types/DataTypes.sol';
+import { IERC20 } from '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
+import { IUniswapV2Router02 } from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import { IAToken } from '@aave/protocol-v2/contracts/interfaces/IAToken.sol';
+
+contract AaveTrage {
+
+    address private uniswap;
+    address private aave;
+
+    constructor (address _uniswapAddress, address _aaveAddress) public {
+        uniswap = _uniswapAddress;
+        aave = _aaveAddress;
+    }
+
+    /*
+        Function peek - Returns the best tokens to borrow and supply for arbitrage
+        Returns: tuple of addresses bestBorrowToken, bestSupplyToken
+    */
+    function peek() external returns (address bestBorrowToken, address bestSupplyToken) {
+        address[] memory reserves = ILendingPool(aave).getReservesList();
+        uint128 maxLend = 1;
+        uint128 minBorrow = uint128(-1);
+
+        for (uint i = 0; i < reserves.length; i++) {
+            DataTypes.ReserveData memory reserveData = ILendingPool(aave).getReserveData(reserves[i]);
+            uint128 borrowAPY = reserveData.currentVariableBorrowRate;
+            uint128 depositAPY = reserveData.currentLiquidityRate;
+
+            if (borrowAPY < minBorrow && borrowAPY > 0) {
+                minBorrow = borrowAPY;
+                bestBorrowToken = reserves[i];
+            }
+
+            if (depositAPY > maxLend) {
+                maxLend = depositAPY;
+                bestSupplyToken = reserves[i];
+            }
+        }
+    }
+
+    function guap(address collateral, uint256 amount, address borrowedAsset, uint256 amountToBorrow, address arbitrageAsset) external {
+        _deposit(collateral, amount);
+        _borrow(borrowedAsset, amountToBorrow);
+
+        uint256 borrowedBalance = IERC20(borrowedAsset).balanceOf(address(this));
+        _swap(borrowedAsset, arbitrageAsset, borrowedBalance);
+
+        uint256 arbitrageBalance = IERC20(arbitrageAsset).balanceOf(address(this));
+        _deposit(arbitrageAsset, arbitrageBalance);
+    }
+
+    function shut(address arbitrageAsset, address borrowedAsset, address collateral, uint256 withdrawAmount) external {
+        _withdraw(arbitrageAsset, type(uint).max);
+
+        uint256 arbitrageBalance = IERC20(arbitrageAsset).balanceOf(address(this));
+        _swap(arbitrageAsset, borrowedAsset, arbitrageBalance);
+
+        uint256 borrowedBalance = IERC20(borrowedAsset).balanceOf(address(this));
+        uint256 payback = _repay(borrowedAsset, borrowedBalance);
+
+        _withdraw(collateral, withdrawAmount);
+        IERC20(collateral).transfer(msg.sender, withdrawAmount);
+    }
+
+    function _repay(address asset, uint256 amount) internal returns (uint256 payback) {
+        IERC20(asset).approve(aave, amount);
+        payback = ILendingPool(aave).repay(asset, amount, 2, address(this));
+    }
+
+    function _withdraw(address asset, uint256 amount) internal {
+        ILendingPool(aave).withdraw(asset, amount, address(this));
+    }
+
+    function _deposit(address collateral, uint256 amount) internal {
+        IERC20(collateral).approve(aave, amount);
+        ILendingPool(aave).deposit(collateral, amount, address(this), 0);
+    }
+
+    function _borrow(address assetToBorrow, uint256 amount) internal {
+        ILendingPool(aave).borrow(assetToBorrow, amount, 2, 0, address(this));
+    }
+
+    function _swap(address from, address to, uint256 amount) internal {
+        IERC20(from).approve(uniswap, amount);
+        address[] memory path = new address[](2);
+        path[0] = from;
+        path[1] = to;
+        IUniswapV2Router02(uniswap).swapExactTokensForTokens(amount, 0, path, address(this), block.timestamp + 10 ** 2);
+    }
+}
---
+++ test/test_aavetrage.js
@@ -0,0 +1,75 @@
+const AaveTrage = artifacts.require('AaveTrage');
+const uniswap = require('@uniswap/v2-periphery/build/UniswapV2Router02.json');
+const weth = require('@uniswap/v2-periphery/build/ERC20.json');
+const aave = require('@aave/protocol-v2/artifacts/contracts/protocol/lendingpool/LendingPool.sol/LendingPool.json');
+const _ = require('lodash')
+
+//MAIN NETWORK
+const UNISWAP_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+const AAVE_ADDRESS = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9';
+
+contract('Aave-Trage', accounts => {
+    const user1 = accounts[0];
+    const user2 = accounts[1];
+    let UNISWAP;
+    let AAVE;
+    let AAVETRAGE;
+
+    beforeEach(async () => {
+        UNISWAP = new web3.eth.Contract(uniswap.abi, UNISWAP_ADDRESS);
+        AAVE = new web3.eth.Contract(aave.abi, AAVE_ADDRESS);
+        AAVETRAGE = await AaveTrage.new(UNISWAP_ADDRESS, AAVE_ADDRESS);
+    })
+
+    it('Should perform peek correctly', async () => {
+        const { bestBorrowToken, bestSupplyToken } = await AAVETRAGE.peek.call();
+
+        const reserves = await AAVE.methods.getReservesList().call();
+        const reservesInfo = await Promise.all(reserves.map(address => AAVE.methods.getReserveData(address).call()));
+        let borrowManual;
+        let supplyManual;
+        let supplyMax = -Infinity;
+        let borrowMin = Infinity;
+
+        for (i = 0; i < reserves.length; i++) {
+            const borrowAPY = Number(reservesInfo[i]['currentVariableBorrowRate']);
+            const supplyAPR = Number(reservesInfo[i]['currentLiquidityRate']);
+
+            if (borrowAPY < borrowMin && borrowAPY > 0) {
+                borrowMin = borrowAPY;
+                borrowManual = reserves[i];
+            }
+
+            if (supplyAPR > supplyMax) {
+                supplyMax = supplyAPR;
+                supplyManual = reserves[i];
+            }
+        }
+
+        assert.equal(bestBorrowToken.toLowerCase(), borrowManual.toLowerCase(), 'Borrow tokens mismatch!');
+        assert.equal(bestSupplyToken.toLowerCase(), supplyManual.toLowerCase(), 'Supply tokens mismatch!');
+    });
+
+    it('Should perform guap and shut correctly', async () => {
+        const wrappedETHaddress = await UNISWAP.methods.WETH().call();
+        const daiAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+        const chainLink = '0x514910771af9ca656af840dff83e8264ecf986ca';
+        const TEST_AMOUNT = web3.utils.toWei('1', "ether");
+        const AMOUNT_TO_BORROW = web3.utils.toWei('2000', 'ether');
+        const WITHDRAW_AMOUNT = '1000';
+
+        //Convert ETH to WETH
+        const WETH = await new web3.eth.Contract(weth.abi, wrappedETHaddress);
+        await web3.eth.sendTransaction({from: user1, to: wrappedETHaddress, value:TEST_AMOUNT});
+        await WETH.methods.transfer(AAVETRAGE.address, TEST_AMOUNT).send({from: user1});
+
+        await AAVETRAGE.guap(wrappedETHaddress, TEST_AMOUNT, daiAddress, AMOUNT_TO_BORROW, chainLink, {from: user1});
+        await AAVETRAGE.shut(chainLink, daiAddress, wrappedETHaddress, WITHDRAW_AMOUNT);
+
+        const finalBalance = await WETH.methods.balanceOf(user1).call();
+        assert.equal(WITHDRAW_AMOUNT, finalBalance);
+
+        //Needed to run the test again
+        await WETH.methods.transfer(daiAddress, finalBalance).send({from: user1});
+    })
+});
